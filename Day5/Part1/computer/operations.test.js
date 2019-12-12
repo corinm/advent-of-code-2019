1fc5c5ae6802114af16d849beb4d1bd9d039878d
Corin <corin.mulliss@strongbyte.com>
2019-12-12 20:50:33 +0000

    5.1: Fixed add and multiply tests

--- Day5/Part1/computer/operations.test.js
+++ Day5/Part1/computer/operations.test.js
@@ -31,8 +31,15 @@ describe('add', () => {
     })
   })
 
-  xdescribe('immediate mode', () => {
-    test('correctly adds', () => {
+  describe('immediate mode', () => {
+    test('correctly adds (4 in pos 1 + 3 in pos 2 = 7 in pos 4)', () => {
+      const opcode = [1101, 4, 3, 4, 33]
+      expect(add(opcode, 0)).toEqual([1101, 4, 3, 4, 7])
+    })
+
+    test('correctly adds (33 in pos 4 + 3 in pos 2 = 36 in pos 4)', () => {
+      const opcode = [1001, 4, 3, 4, 33]
+      expect(add(opcode, 0)).toEqual([1001, 4, 3, 4, 36])
     })
   })
 })
@@ -55,14 +62,14 @@ describe('multiply', () => {
   })
 
   describe('immediate mode', () => {
-    test('correctly multiplies (4 * 3 = 12 in pos 4)', () => {
-      const opcode = [1102, 4, 3, 4, 33]
+    test('correctly multiplies (4 in pos 1 * 3 in pos 2 = 12 in pos 4)', () => {
+      const opcode = [1102, 4, 3, 4, 1]
       expect(multiply(opcode, 0)).toEqual([1102, 4, 3, 4, 12])
     })
 
-    test('correctly multiplies (4 * 3 = 12 in pos 4)', () => {
-      const opcode = [1002, 4, 3, 4, 33]
-      expect(multiply(opcode, 0)).toEqual([1002, 4, 3, 4, 99])
+    test('correctly multiplies (2 in pos 4 * 4 in pos 3 = 16 in pos 4)', () => {
+      const opcode = [102, 4, 3, 4, 2]
+      expect(multiply(opcode, 0)).toEqual([102, 4, 3, 4, 16])
     })
   })
 })
