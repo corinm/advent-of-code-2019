e7342a85f7dd525a1847f9668f4bfe27c2c665e1
Corin <corin.mulliss@gmail.com>
2019-12-12 23:36:30 +0000

    5.2: Added more tests

--- Day5/Part2/computer/index.test.js
+++ Day5/Part2/computer/index.test.js
@@ -49,38 +49,86 @@ describe('runOpcode', () => {
     expect(finalOpcode).toEqual([30, 1, 1, 4, 2, 5, 6, 0, 99])
   })
 
-  test('5.2 example 1 - return 0 as 1 is not equal to 8', async () => {
-    const opcode = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
-    const inputs = [1]
-    const { outputs } = await runOpcode(opcode, inputs)
-    expect(outputs).toEqual([0, 'EXIT'])
-  })
+  describe('task 5.2', () => {
+    describe('position mode', () => {
+      describe('equals', () => {
+        test('input 8 equals 8 therefore return 1', async () => {
+          const opcode = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+          const inputs = [8]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([1, 'EXIT'])
+        })
 
-  test('5.2 example 1 - return 1 as 8 is equal to 8', async () => {
-    const opcode = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
-    const inputs = [8]
-    const { outputs } = await runOpcode(opcode, inputs)
-    expect(outputs).toEqual([1, 'EXIT'])
-  })
+        test('input 1 does not equal 8 therefore return 0', async () => {
+          const opcode = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+          const inputs = [1]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([0, 'EXIT'])
+        })
+      })
 
-  test('5.2 example 2 - return 1 as 7 is less than 8', async () => {
-    const opcode = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
-    const inputs = [7]
-    const { outputs } = await runOpcode(opcode, inputs)
-    expect(outputs).toEqual([1, 'EXIT'])
-  })
+      describe('less than', () => {
+        test('input 1 less than 8 so return 1', async () => {
+          const opcode = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+          const inputs = [7]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([1, 'EXIT'])
+        })
+        test('input 9 is not less than 9 so return 0', async () => {
+          const opcode = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+          const inputs = [9]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([0, 'EXIT'])
+        })
 
-  test('5.2 example 2 - return 0 as 9 is not less than 8', async () => {
-    const opcode = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
-    const inputs = [9]
-    const { outputs } = await runOpcode(opcode, inputs)
-    expect(outputs).toEqual([0, 'EXIT'])
-  })
+        test('input 8 is not less than 9 so return 0', async () => {
+          const opcode = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+          const inputs = [8]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([0, 'EXIT'])
+        })
+      })
+    })
 
-  test('5.2 example 2 - return 0 as 8 is not less than 8', async () => {
-    const opcode = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
-    const inputs = [8]
-    const { outputs } = await runOpcode(opcode, inputs)
-    expect(outputs).toEqual([0, 'EXIT'])
+    describe('immediate mode', () => {
+      describe('equals', () => {
+        test('input 8 equals 8 so return 1', async () => {
+          const opcode = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+          const inputs = [8]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([1, 'EXIT'])
+        })
+
+        test('input 7 is not equal to 8 so return 0', async () => {
+          const opcode = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+          const inputs = [7]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([0, 'EXIT'])
+        })
+      })
+
+      describe('lessThan', () => {
+        test('input 7 is less than 8 so return 1', async () => {
+          const opcode = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+          const inputs = [7]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([1, 'EXIT'])
+        })
+
+        test('input 9 is not less than 8 so return 0', async () => {
+          const opcode = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+          const inputs = [9]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([0, 'EXIT'])
+        })
+
+        test('input 8 is not less than 8 so return 0', async () => {
+          const opcode = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+          const inputs = [8]
+          const { outputs } = await runOpcode(opcode, inputs)
+          expect(outputs).toEqual([0, 'EXIT'])
+        })
+      })
+    })
   })
 })
